9a18c9035c19fd4cc4e5e418b5a8ed19df0f0537
Jiří Prokop <jprokop@synaptiko.cz>
2019-07-02 20:53:22 +0200

    Add my initial Planck EZ keymap

---
+++ keyboards/planck/keymaps/synaptiko/rules.mk
@@ -0,0 +1,23 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+NKRO_ENABLE = yes
+QMK_KEYS_PER_SCAN = yes
+
+RGBLIGHT_ANIMATION = no
+RGBLIGHT_ENABLE = no
+UNICODE_ENABLE = no
+API_SYSEX_ENABLE = no
+AUTOLOG_ENABLE = no
+BOOTMAGIC_ENABLE = no
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
+DEBUG_ENABLE = no
+FORCE_NKRO = no
+KEY_LOCK_ENABLE = no
+KEYLOGGER_ENABLE = no
+SLEEP_LED_ENABLE = no
+SWAP_HANDS_ENABLE = no
+TAP_DANCE_ENABLE = no
