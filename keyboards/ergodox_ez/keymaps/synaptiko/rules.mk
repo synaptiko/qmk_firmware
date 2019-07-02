d964fcb843a02f2c9b06e50f1656cd7fb7350236
Jiří Prokop <jprokop@synaptiko.cz>
2019-07-02 21:11:22 +0200

    Update formatting according to the .editorconfig file

--- keyboards/ergodox_ez/keymaps/synaptiko/rules.mk
+++ keyboards/ergodox_ez/keymaps/synaptiko/rules.mk
@@ -1,3 +1,5 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 EXTRAKEY_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 NKRO_ENABLE = yes
