94e9d537858c3e8d5fb863bcc440d8b2ebdfda94
Jiří Prokop <jprokop@synaptiko.cz>
2019-08-11 14:46:24 +0200

    Fix after rebase

--- keyboards/planck/keymaps/synaptiko/config.h
+++ keyboards/planck/keymaps/synaptiko/config.h
@@ -7,6 +7,5 @@
 
 #define NO_MUSIC_MODE
 #define RGB_DISABLE_WHEN_USB_SUSPENDED true
-#define EECONFIG_RGB_MATRIX (uint32_t *)16
 #define RGB_MATRIX_LED_FLUSH_LIMIT 100
 #define RGB_DISABLE_AFTER_TIMEOUT 50 // about a minute
